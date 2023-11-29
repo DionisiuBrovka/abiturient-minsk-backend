0d485325cdaf5cad5f83a7420c47b89325bd70e6
Dionisiu Brovka <dev.dionisiu.brovka@gmail.com>
2023-11-29 20:54:51 +0300

    edit admin site

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.site_header = 'Адним панель "Приложения коляг"'
 
 # Register your models here.
+
+@admin.register(Establishment)
+class EstablishmentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'adress']
+    list_display_links = ['id', 'title']
+
+    search_fields = ['title', 'short_title']
+
+@admin.register(SpecialtyGroup)
+class SpecialtyGroupAdmin(admin.ModelAdmin):
+    list_display = ['id', 'code', 'title', 'g_type']
+    list_display_links = ['id', 'code', 'title']
+    list_filter = ['g_type']
+
+    search_fields = ['code', 'title']
+
+@admin.register(Specialty)
+class SpecialtyAdmin(admin.ModelAdmin):
+    list_display = ['id', 'code', 'title', 'skill', 'group']
+    list_display_links = ['id', 'code', 'title', 'skill']
+
+    search_fields = ['code', 'title', 'skill']
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'c_type']
+    list_display_links = ['title', 'c_type']
--- api/models.py
+++ api/models.py
@@ -9,6 +9,7 @@ class Establishment(models.Model):
     desc = models.TextField(null=True, blank=True, verbose_name="Описание")
     schedule = models.TextField(null=True, blank=True, verbose_name="График работы")
     contacts = models.ManyToManyField('Contact', verbose_name="Контакты")
+    adress = models.CharField(max_length=255, null=False, blank=False, verbose_name="Адрес")
 
     specialty = models.ManyToManyField('Specialty', verbose_name="Специальности")
 
@@ -17,6 +18,9 @@ class Establishment(models.Model):
     promo_medio = models.FileField(null=True, blank=True, verbose_name="Промо ролик")
 
     coords = models.CharField(max_length=255, null=True, blank=True, verbose_name="Координаты на карте") 
+
+    def __str__(self) -> str:
+        return self.short_title
 
     class Meta:
         verbose_name = "Образовательное учреждение"
@@ -36,6 +40,9 @@ class SpecialtyGroup(models.Model):
     prev = models.ImageField(null=True, blank=True, verbose_name="Превью")
     desc = models.TextField(null=True, blank=True, verbose_name="Описание")
 
+    def __str__(self) -> str:
+        return self.title
+
     class Meta:
         verbose_name = "Группа специальностей"
         verbose_name_plural = "Группы специальностей"
@@ -43,11 +50,14 @@ class SpecialtyGroup(models.Model):
 class Specialty(models.Model):
 
     group = models.ForeignKey('SpecialtyGroup', models.CASCADE, null=False , blank=False, verbose_name="Группа специальностей")
-    code = models.CharField(max_length=50, null=True, blank=True, verbose_name="Код")
+    code = models.CharField(max_length=50, null=False, blank=False, verbose_name="Код")
     title = models.CharField(max_length=255, null=False, blank=False, verbose_name="Название специальности")
     skill = models.CharField(max_length=255, null=False, blank=False, verbose_name="Название квалификации")
     prev = models.ImageField(null=True, blank=True, verbose_name="Превью")
     desc = models.TextField(null=True, blank=True, verbose_name="Описание")
+
+    def __str__(self) -> str:
+        return self.code + " // " + self.title
 
     class Meta:
         verbose_name = "Специальность"
@@ -56,23 +66,26 @@ class Specialty(models.Model):
 class Contact(models.Model):
 
     CCOISES = [
-        ("SIT","Сайт"),
-        ("EMA","Электронная почта"),
-        ("MAI","Древняя почта"),
-        ("PHO","Номер"),
-        ("TEL","Телеграм"),
-        ("VIB","Вайбер"),
+        ("SITE","Сайт"),
+        ("EMAIL","Электронная почта"),
+        ("MAIL","Древняя почта"),
+        ("PHONE","Номер"),
+        ("TELEGRAM","Телеграм"),
+        ("VIBER","Вайбер"),
         ("VK","Вконтакте"),
-        ("INS","Инстаграм"),
-        ("FAC","Фейсбук"),
+        ("INST","Инстаграм"),
+        ("FACEBOOK","Фейсбук"),
     ]
 
     title = models.CharField(max_length=255, null=False, blank=False, verbose_name="Название контакта")
-    c_type = models.CharField(max_length=3, choices=CCOISES, null=False, blank=False, verbose_name="Тип контакта" )
+    c_type = models.CharField(max_length=10, choices=CCOISES, null=False, blank=False, verbose_name="Тип контакта" )
     email = models.EmailField(null=True, blank=True, verbose_name="Почта (если такой тип)")
     url = models.URLField(null=True, blank=True, verbose_name="УРЛ (если такой тип)")
     phone = models.CharField(max_length=20, null=True, blank=True, verbose_name="Телефон (если такой тип)")
     other = models.CharField(max_length=255, null=True, blank=True, verbose_name="Другое (если такой тип)")
+
+    def __str__(self) -> str:
+        return f"{self.title} ({self.c_type})"
 
     class Meta:
         verbose_name = "Контакт"
