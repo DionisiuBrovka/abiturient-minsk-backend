e56e5f7217fd2be38c0f41897f42e896d0692f40
Dionisiu Brovka <dev.dionisiu.brovka@gmail.com>
2024-04-22 16:44:40 +0300

    fix

--- data/models.py
+++ data/models.py
@@ -178,6 +178,8 @@ class SkillForEstablishment(models.Model):
         ("9","На основе общего базового образования (после 9 кл.)"),
         ("11","На основе общего среднего образования (после 11 кл.)"),
         ("ПТО","На основе ПТО"),
+        ("ССО","На основе ССО"),
+        ("Спец","На основе спец. школы"),
     ]
 
     est = models.ForeignKey('Establishment', models.CASCADE, related_name="skills", null=False, blank=False, verbose_name="УО")
